6f91bd3b7e0e35b8aef806703cd7a5a0df0fe71b
Manraj <manrajkhokhar1995@gmail.com>
2020-01-09 12:25:28 -0800

    add slideshow of categories to home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,88 +3,108 @@ import React from 'react';
 class Home extends React.Component{
     constructor(props){
         super(props);
+        this.state = { slideIndex: 0 };
 
         this.incrementIndex = this.incrementIndex.bind(this);
+        this.currentSlide = this.currentSlide.bind(this);
         
     }
     componentDidMount(){
-        this.slideIndex = 0;
-        this.showSlides();
-        setInterval(this.incrementIndex, 2000);
+        setInterval(this.incrementIndex, 4000);
     }
 
     incrementIndex(){
-        if (this.slideIndex < 9) {
-            this.slideIndex += 1;
+        const dot = document.getElementsByClassName('dot');
+        if (this.state.slideIndex < 8) {
+            this.setState({slideIndex: this.state.slideIndex+1}, () => {
+                dot[this.state.slideIndex-1].classList.toggle("active");
+                dot[this.state.slideIndex].classList.toggle("active");
+            });
         } else {
-            this.slideIndex = 0;
+            this.setState({ slideIndex: 0 }, () => {
+                dot[8].classList.toggle("active");
+                dot[this.state.slideIndex].classList.toggle("active");
+            });
         }
     }
+    
+    currentSlide(index){
+        const dot = document.getElementsByClassName('dot');
+        const previousIndex = this.state.slideIndex;
+        this.setState({slideIndex: index}, () => {
+            dot[previousIndex].classList.toggle("active");
+            dot[this.state.slideIndex].classList.toggle("active");
+        });
+        this.setState({});
+    }
     render(){
+        
         const mySlides = [
-            <div className="mySlides fade">
-                <img src={window.business} />
+            (<div className="mySlides fade">
                 <div className="text">Business</div>
-            </div>,
+                <img src={window.business} />
+            </div>),
 
-            <div className="mySlides fade">
-                <img src={window.education} />
+            (<div className="mySlides fade">
                 <div className="text">Education</div>
-            </div>,
+                <img src={window.education} />
+            </div>),
 
-            <div className="mySlides fade">
-                <img src={window.food} />
+            (<div className="mySlides fade">
                 <div className="text">Food</div>
-            </div>,
+                <img src={window.food} />
+            </div>),
 
-            <div className="mySlides fade">
-                <img src={window.health} />
+            (<div className="mySlides fade">
                 <div className="text">Health</div>
-            </div>,
+                <img src={window.health} />
+            </div>),
 
-            <div className="mySlides fade">
+            (<div className="mySlides fade">
+                <div className="text">LGBTQIAA</div>
                 <img src={window.lgbt} />
-                <div className="text">LGBTQIA</div>
-            </div>,
+            </div>),
 
-            <div className="mySlides fade">
-                <img src={window.psychology} />
+            (<div className="mySlides fade">
                 <div className="text">Psychology</div>
-            </div>,
-            <div className="mySlides fade">
-                <img src={window.relationships} />
+                <img src={window.psychology} />
+            </div>),
+            (<div className="mySlides fade">
                 <div className="text">Relationships</div>
-            </div>,
+                <img src={window.relationships} />
+            </div>),
 
-            <div className="mySlides fade">
-                <img src={window.technology} />
+            (<div className="mySlides fade">
                 <div className="text">Technology</div>
-            </div>,
+                <img src={window.technology} />
+            </div>),
 
-            <div className="mySlides fade">
-                <img src={window.work} />
+            (<div className="mySlides fade">
                 <div className="text">Work</div>
-            </div>
+                <img src={window.work} />
+            </div>)
         ];
         return (
-            <div>
-                <h1>Get smarter about what matters to you</h1>
+            <div className='main-home-container'>
+                <div className="home-header-container">
+                    <h1 className="home-header" >Get smarter about what matters to you.</h1>
+                </div>
                 
                 <div className="slideshow-container">
-                    {
-                        mySlides[this.slideIndex]
-                    }
-                    {/* <div >
-                        <span className="dot" onClick={this.currentSlide(1)}></span>
-                        <span className="dot" onClick={this.currentSlide(2)}></span>
-                        <span className="dot" onClick={this.currentSlide(3)}></span>
-                        <span className="dot" onClick={this.currentSlide(4)}></span>
-                        <span className="dot" onClick={this.currentSlide(5)}></span>
-                        <span className="dot" onClick={this.currentSlide(6)}></span>
-                        <span className="dot" onClick={this.currentSlide(7)}></span>
-                        <span className="dot" onClick={this.currentSlide(8)}></span>
-                        <span className="dot" onClick={this.currentSlide(9)}></span>
-                    </div> */}
+                    
+                    {mySlides[this.state.slideIndex]}
+                            
+                </div>
+                <div className='dots-container' >
+                    <span className="dot active" onClick={() => this.currentSlide(0)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(1)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(2)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(3)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(4)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(5)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(6)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(7)}></span>
+                    <span className="dot" onClick={() => this.currentSlide(8)}></span>
                 </div>
                 <div>Get Started</div>
                 <div>Checklist</div>
