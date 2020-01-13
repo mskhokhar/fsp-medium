8b71c46f12e662b39aa586c1c2587b52b9f17a8e
Manraj <manrajkhokhar1995@gmail.com>
2020-01-12 19:10:16 -0800

    add post component details

--- app/views/api/posts/_show.json.jbuilder
+++ app/views/api/posts/_show.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! post, :id, :author_id, :title, :body, :category_id
+json.extract! post, :id, :author_id, :title, :body, :category_id, :updated_at
 json.photoUrl url_for(post.picture)
--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,10 +9,35 @@ import FeedContainer from '../components/feed/feed_container'
 
 const App = () => (
     <div>
-        < Modal/>
-        < NavbarContainer />
-        < AuthRoute path='/' component={Home}/>
-        < ProtectedRoute path='/feed' component={FeedContainer}/>
+        <div class='main-container'>
+            < Modal />
+            < NavbarContainer />
+            < AuthRoute path='/' component={Home} />
+            < Route path='/feed' component={FeedContainer} />
+        </div>
+        <footer >
+            <div>
+                <div class="footer-info">
+                    <div><a href="">Blogium</a></div>
+                    <div class="footer-info-links">
+                        <div><a href="">Get Started.</a></div>
+                        <div><span>Have an account?      </span><a href="">Sign In</a></div>
+                    </div>
+                    <div class="footer-info-additional">
+                        <div><a href="https://github.com/mskhokhar/fsp-medium">Github repository.</a></div>
+                        <div><a href="https://www.linkedin.com/in/manraj-singh-01366710a">LinkedIn profile link.</a></div>
+                    </div>
+                </div>
+                <div class="logo-container">
+                    <div class="logo-links">
+                        <a href="https://github.com/mskhokhar/fsp-medium"><img src={window.github} alt="github"/></a>
+                    </div>
+                    <div class="logo-links">
+                        <a href="https://www.linkedin.com/in/manraj-singh-01366710a"><img src={window.linkedIn} alt="linkedIn" /></a>
+                    </div>
+                </div>
+            </div>
+            </footer> 
     </div>
 );
 
